b27a7783f634ec8c9b758f8acb2ebd129544d805
xiaoshuai1127 <1106505519@qq.com>
2016-01-06 17:36:04 +0800

    aa

---
+++ src/test/java/com/sunms/domain/Test.java
@@ -0,0 +1,28 @@
+package com.sunms.domain;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String resource = "conf.xml";
+		try {
+			Reader reader = Resources.getResourceAsReader(resource);
+			SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(reader);
+			SqlSession sqlSession = factory.openSession();
+			String statement = "com.sunms.mappers.user.getUser";
+			User user = sqlSession.selectOne(statement, 1);
+			System.out.println(user);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
