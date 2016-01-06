12c0e1415ddb5637d2bc8f72a61768cb8b40c74e
Sunms <1106505519@qq.com>
2016-01-07 07:14:55 +0800

    home

--- src/main/resources/users.sql
+++ src/main/resources/users.sql
@@ -1,5 +1,5 @@
-create database mybatis;
-use mybatis;
+create database javaframework;
+use javaframework;
 CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(20), age INT);
 INSERT INTO users(NAME, age) VALUES('xiaoshuai', 23);
 INSERT INTO users(NAME, age) VALUES('sunms', 24);
